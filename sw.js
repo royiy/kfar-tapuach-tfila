d4d590e6c3dc45583cca569702f555e187d517e1
Royi Yahud <88856680+royiy@users.noreply.github.com>
2026-03-18 11:15:23 +0200

    Fix

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-self.addEventListener('fetch', () => {});
+// קוד מינימלי חובה כדי שהדפדפן יכיר בזה כאפליקציה
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+    // השארת הפונקציה ריקה מספיקה כדי לעקוף את הבדיקה
+});
